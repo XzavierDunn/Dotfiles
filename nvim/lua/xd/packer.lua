553fbdb6bc204d524c2123555027cbb6ca42a58d
Xzavier Dunn <xmandunn@gmail.com>
2023-05-09 20:00:22 -0400

    markdown-preview

--- nvim/lua/xd/packer.lua
+++ nvim/lua/xd/packer.lua
@@ -41,6 +41,14 @@ return require('packer').startup(function(use)
     }
 }
 
+-- https://github.com/iamcco/markdown-preview.nvim
+use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+})
+
+use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
 -- colorschemes 
 use({ 'rose-pine/neovim', as = 'rose-pine'})
 
